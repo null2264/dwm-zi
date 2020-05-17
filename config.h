b567e054c968b87b05e15fe403b348c245c7c979
ziro <palembani@gmail.com>
2020-05-17 09:31:11 +0700

    + Patched gaps

--- config.h
+++ config.h
@@ -2,6 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const int gappx     = 5;                 /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -83,8 +84,11 @@ static Key keys[] = {
 	{ MODKEY|ALTKEY,		XK_b,	   spawn,	   SHCMD("firefox") },
 	{ MODKEY, 			XK_Print,  spawn,	   SHCMD("~/.scripts/ss.sh") },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
-/*	{ MODKEY|ALTKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY|ALTKEY,                       XK_k,      focusstack,     {.i = -1 } },*/
+/*	{ MODKEY|ALTKEY,                XK_j,      focusstack,     {.i = +1 } },
+	{ MODKEY|ALTKEY,                XK_k,      focusstack,     {.i = -1 } },*/
+	{ MODKEY|ALTKEY,                XK_h,	   setgaps,        {.i = -5 } },
+	{ MODKEY|ALTKEY,                XK_l,	   setgaps,        {.i = +5 } },
+	{ MODKEY|ALTKEY|ShiftMask,      XK_equal,  setgaps,        {.i = 0  } },
 	STACKKEYS(MODKEY,                          focus)
 	STACKKEYS(MODKEY|ShiftMask,                push)
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
